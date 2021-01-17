87e3373e117288ffe513e5cd8692cb267b0c092f
Jan Kamienicki <nowak480@gmail.com>
2021-01-17 10:22:49 +0100

    created base project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 3000);
