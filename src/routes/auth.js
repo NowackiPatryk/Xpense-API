352d17192f4e8a5027b78e4b8278347f7ab1346f
NowackiPatryk <41294605+NowackiPatryk@users.noreply.github.com>
2021-01-17 18:26:22 +0100

    Removed empty spaces in auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,13 +2,11 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-
 const User = require("../schemas/user");
 
 router.post("/auth/register", async (req, res) => {
   const { email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
-
   const response = await User.create({
     email: email,
     password: hashedPassword,
