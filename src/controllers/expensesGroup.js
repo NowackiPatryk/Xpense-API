6e76ac425a521a597f8c28c2a4fa29f74b859317
Jan Kamienicki <nowak480@gmail.com>
2021-01-18 15:36:12 +0100

    created expensesGroup controller

---
+++ src/controllers/expensesGroup.js
@@ -0,0 +1,23 @@
+const ExpensesGroup = require("../models/expensesGroup");
+
+exports.getAll = async (req, res) => {
+    const { tokenData } = req.decodedToken;
+    const expensesGroups = await ExpensesGroup.find({ userId: tokenData.id });
+
+    res.json({ expensesGroups });
+};
+
+exports.create = async (req, res) => {
+    const { tokenData } = req.decodedToken;
+    const { name, expenses } = req.body;
+
+    const expensesGroup = await ExpensesGroup.create({
+        userId: tokenData.id,
+        name: name,
+        expenses: expenses,
+    }).catch((err) => {
+        res.json({ status: "error", error: err.toString() });
+    });
+
+    res.json({ status: "success" });
+};
